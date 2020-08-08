0282c1bd4e092a4bde1c7f67a8b285113c220fc9
Otavio Lemos <otaviolemos@gmail.com>
2020-08-08 10:11:43 -0300

    test: add 3444 test

--- src/roman.ts
+++ src/roman.ts
@@ -17,33 +17,36 @@ export function toRoman (n: number): string {
 }
 
 function getRomanNumeral (place: number, digit: number): string {
-  const placeSymbols = ['I', 'X', 'C', 'M']
-  const placeHalfSymbols = ['V', 'L', 'D']
+  const placeSymbols = ['I', 'X', 'C', 'M', '']
+  const placeHalfSymbols = ['V', 'L', 'D', '']
   let ret = ''
+  const symbol = placeSymbols[place]
+  const halfSymbol = placeHalfSymbols[place]
+  const nextSymbol = placeSymbols[place + 1]
 
   switch (digit) {
     case 1:
     case 2:
     case 3:
-      ret = placeSymbols[place]
+      ret = symbol
       for (let i = 1; i < digit; i++) {
-        ret += placeSymbols[place]
+        ret += symbol
       }
       break
     case 4:
-      ret = placeSymbols[place] + placeHalfSymbols[place]
+      ret = symbol + halfSymbol
       break
     case 5:
     case 6:
     case 7:
     case 8:
-      ret = placeHalfSymbols[place]
+      ret = halfSymbol
       for (let i = 0; i < digit - 5; i++) {
-        ret += placeSymbols[place]
+        ret += symbol
       }
       break
     case 9:
-      ret = placeSymbols[place] + placeSymbols[place + 1]
+      ret = symbol + nextSymbol
   }
 
   return ret
