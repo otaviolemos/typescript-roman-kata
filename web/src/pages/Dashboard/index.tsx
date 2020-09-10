7b29a671449d8ff9e047d21c7432c8124bc17423
Wemerson Jesus <wejesuss@gmail.com>
2020-09-10 17:39:23 -0300

    chore: add roman converter components

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Landing from "../../components/Landing";
+import Converter from "../../components/Converter";
 import { Container } from "./styles";
 
 const Dashboard: React.FC = () => {
   return (
     <Container>
       <Landing />
+      <Converter />
     </Container>
   );
 };
