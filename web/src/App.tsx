6050fe2003f70b82015f2ef194a516f39f6ad3ea
ARTHURPC03 <arthurpimetelcordeiro@gmail.com>
2020-09-10 13:48:23 -0300

    Theme Switcher + Eslint, Prettier & EditorConfig

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import { BrowserRouter as Router } from 'react-router-dom'
+import usePersistedState from './utils/usePersistedState'
 
-function App() {
+import ThemeSwitcher from './components/ThemeSwitcher'
+import light from './styles/themes/light'
+import dark from './styles/themes/dark'
+
+import Routes from './routes'
+
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark)
+
+  const toggleTheme = (): void => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
   return (
-    <h1>Hello World</h1>
-  );
+    <ThemeProvider theme={theme}>
+      <Router>
+        <ThemeSwitcher toggleTheme={toggleTheme} />
+        <Routes />
+      </Router>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ web/src/components/ThemeSwitcher/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 10vh;
+  width: 100%;
+  flex: 1;
+  background: ${props => props.theme.colors.li};
+  h1 {
+    align-content: space-between;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+`
