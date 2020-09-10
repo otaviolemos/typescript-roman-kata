3194d0b8c5600a9643822e5eb1fac71352085341
Wemerson Jesus <wejesuss@gmail.com>
2020-09-10 17:38:29 -0300

    style: limits the height of the components

--- web/src/components/ThemeSwitcher/styles.tsx
+++ web/src/components/ThemeSwitcher/styles.tsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 10vh;
-  width: 100%;
-  flex: 1;
   background: ${(props) => props.theme.colors.li};
   h1 {
     align-content: space-between;
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100vh;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   flex: 1;
   background: ${(props) => props.theme.colors.background};
 `;
